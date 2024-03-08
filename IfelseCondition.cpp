dbc19e5ffcd186892935822a9f6321fa4df80763
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-03-08 22:15:50 +0530

    [ADDED]:shubham This code for if-else conditional Statement

---
+++ IfelseCondition.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+void main()
+{
+	int n;
+	cout<<"Enter the Any number="<<endl;
+	cin>>n;
+	if(n % 2 == 0)
+	{
+		cout<<"Number is Even"<<endl;
+	}
+	else{
+		cout<<"Number is Odd"<<endl;
+	}
+}
