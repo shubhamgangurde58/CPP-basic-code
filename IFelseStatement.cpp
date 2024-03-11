e4a8733c4419493e780bff1579b06efed288278a
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-03-11 23:05:23 +0530

    [ADDED]:shubham This code for If else statement

---
+++ IFelseStatement.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cout<<"Enter ant no";
+	cin>>n;
+	if(n % 2 == 0)
+	{
+		cout<<"number is Even";
+	}else{
+		cout<<"number is odd";
+	}
+}
