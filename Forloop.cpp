9a3e7295d03a832afa2d5f4c0609c3b90ef53c3c
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-03-11 22:54:06 +0530

    [ADDED]:SHUBHAM this code for loop:

---
+++ Forloop.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	int n;
+	int i;
+	cout<<"Enter any no:";
+	cin>>n;
+	
+	for(int i=0; i<n ; i++){
+		cout<<"shubham gangurde"<<endl;
+	}
+	return 0;
+}
