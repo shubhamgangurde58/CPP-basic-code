a7e71b5771f569e81f5d1777ec0f30341697d9f9
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-04-08 21:41:30 +0530

    [ADDED]:shubham This code while loop demo

---
+++ While_loopDemo.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n=0;
+	int i=0;
+	cout<<"Enter a no:"<<endl;
+	cin>>n;
+	while(i<n)
+	{
+		cout<<"Linex Torvalds"<<endl;
+		i++;
+	}
+	return 0;
+}
