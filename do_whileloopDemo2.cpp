679a2b27ed4aa4eba583f655efb27f8a08aa8af4
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-04-22 18:35:19 +0530

    [ADDED]:shubham This code for do while loop Demo2

---
+++ do_whileloopDemo2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cout<<"Enter a no"<<endl;
+	cin>>n;
+	int i=0;
+	
+	do{
+		cout<<"\nshubham gangurde";
+		i++;
+	}while(i<n);
+	return 0;
+}
