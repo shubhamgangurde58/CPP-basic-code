ad818af448a19687ae4e58f033b6de7c578f943c
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-04-22 18:32:54 +0530

    [ADDED]:shubham This code for loop Demo2

---
+++ For_loopDemo2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	int i;
+	cout<<"Enter the no:"<<endl;
+	cin>>n;
+	for(i=0;i<n;i++)
+	{
+		cout<<"Mark Zuckerberg"<<endl;
+	}
+	return 0;
+}
