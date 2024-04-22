9e860160240364a65db15d4625823bd9a13e40cf
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-04-22 18:34:09 +0530

    [ADDED]:shubham This code for While loop Demo2

---
+++ While_loopDemo2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	int i=0;
+	cout<<"Enter the no:"<<endl;
+	cin>>n;
+	
+	while(i<n)
+	{
+		cout<<"IMRD"<<endl;
+		i++;
+	}
+	return 0;
+}
