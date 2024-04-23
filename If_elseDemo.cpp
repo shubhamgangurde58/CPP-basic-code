386c8fe1be1190bb6e57e827b98c6ae8cc39912a
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-04-23 21:57:19 +0530

    [ADDED]:shubham  this code conditional Statement Demo

---
+++ If_elseDemo.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"Enter the any no:"<<endl;
+	cin>>n;
+	
+	if(n%2==0)
+	{
+		cout<<"Number is even"<<endl;
+	}
+	else{
+		cout<<"number is odd"<<endl;
+	}
+	return 0;
+}
