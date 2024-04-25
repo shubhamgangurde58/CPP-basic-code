f356644eed8c0003594b14a126cdf0377fc9b968
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-04-25 22:02:06 +0530

    [ADDED]:shubham This code for Arithmatic opretion demo

---
+++ Arithmetice_opreDemo.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,c,d,e,f;
+	cout<<"\nEnter The value of A=";
+	cin>>a;
+	cout<<"\nEnter the value of B=";
+	cin>>b;
+	c=a+b;
+	cout<<"\nAddition="<<c;
+	d=a-b;
+	cout<<"\nSubtraction="<<d;
+	e=a*b;
+	cout<<"\nMultiplication="<<e;
+	f=a/b;
+	cout<<"\nDivision="<<f;
+	return 0;
+}
