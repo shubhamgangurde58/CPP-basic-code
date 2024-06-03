31bf48b2629bce143853acd64c4901ee9334b84c
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-06-03 22:19:34 +0530

    [ADDED]: shubham This code for virtual method is declare =0;

---
+++ VirtualFunctionDemo2.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+class Base{
+	public:
+		
+		virtual  void show()=0;
+		
+		void display(){
+			
+			cout<<"display of Base"<<endl;
+		}	
+};
+class Derived: public Base{
+	
+		public:
+		void show(){
+			
+			cout<<"show of Derived:"<<endl;
+		}
+		void display(){
+			
+			cout<<"display of Derived"<<endl;
+		}	
+};
+
+int main(){
+	
+	Base *objB;
+
+	Derived objD;
+	objB = &objD;
+	
+	objB->show();
+	objB->display();
+	
+//	objD.show();
+//	objD.display();
+	return 0;
+}
