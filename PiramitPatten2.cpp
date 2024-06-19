a9f7dff45181473f9e2de08eb73900fafffe5bf7
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-06-19 21:48:19 +0530

    [ADDED]: shubham this code for Draw Piramid Patten 2

---
+++ PiramitPatten2.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+
+	for(int i=0;i<=5;i++)
+	{
+		int num = 0;
+			for(int j=5;j>=0;j--)
+			{
+				if(j<=i)
+				{
+					cout<<num++ <<"";
+				}
+				
+					cout<<" ";		
+			}
+			
+			cout<<"\n";
+	}
+	return 0;
+}
