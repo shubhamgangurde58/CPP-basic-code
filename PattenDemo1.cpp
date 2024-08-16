5a41ec983a0ded3cda1877690a7dfcb1c54d1209
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-08-16 18:39:50 +0530

    [ADDED]: shubham This code for Print Patten solid rectangle

---
+++ PattenDemo1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace::std;
+int main(){
+	
+	//print solid rectangle;
+	int n=5;
+	for(int i=0;i<n;i++){
+		
+		for(int j=0;j<n;j++){
+			
+			cout<<"* ";
+		}
+		cout<<endl;
+	}
+	
+}
